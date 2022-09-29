797d26cfd91d86057aca366e1169060de2722f21
Jerry Chen <jjerry.chen@mail.utoronto.ca>
2022-09-29 12:56:01 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,16 +1,76 @@
+import java.io.File;
+
 public class Driver { 
- public static void main(String [] args) { 
-  Polynomial p = new Polynomial(); 
-  System.out.println(p.evaluate(3)); 
-  double [] c1 = {6,0,0,5}; 
-  Polynomial p1 = new Polynomial(c1); 
-  double [] c2 = {0,-2,0,0,-9}; 
-  Polynomial p2 = new Polynomial(c2); 
-  Polynomial s = p1.add(p2); 
-  System.out.println("s(0.1) = " + s.evaluate(0.1)); 
-  if(s.hasRoot(1)) 
-   System.out.println("1 is a root of s"); 
-  else 
-   System.out.println("1 is not a root of s"); 
- } 
-} 
+	 public static void main(String [] args) { 
+		 
+		 double [] c1 = {1,2,1};
+		 int [] d1 = {2, 1, 0};
+		 Polynomial p1 = new Polynomial(c1, d1); 
+		 for(double x : p1.coefficients) {
+			 System.out.println(x); //should print out 1.0,2.0,1.0
+		 }
+		 System.out.println();
+		 double [] c2 = {6,0,0,5};
+		 int [] d2 = {3,2,1,0};
+		 Polynomial p2 = new Polynomial(c2, d2);
+		 Polynomial p3 = p2.add(p1);
+		 for(double x : p3.coefficients) {
+			 System.out.println(x); //should print out 6.0,1.0,2.0,6.0
+		 }
+		 System.out.println();
+		 for(int x: p3.degrees) {
+			 System.out.println(x); //should print out 3,2,1,0
+		 }
+		 System.out.println();
+		 double [] c4 = {5, 3, 8};
+		 int [] d4 = {4, 2, 0};
+		 Polynomial p4 = new Polynomial(c4, d4);
+		 Polynomial p5 = p4.add(p3);
+		 
+		 for(double x : p5.coefficients) {
+			 System.out.println(x); //should print out 5.0, 4.0, 14.0, 6.0, 2.0
+		 }
+		 System.out.println();
+		 for(int x: p5.degrees) {
+			 System.out.println(x); //should print out 4, 2, 0, 3, 1
+		 }
+		 System.out.println();
+		 
+		 System.out.println(p1.evaluate(3)); //should print out 16.0
+		 System.out.println(p1.hasRoot(3)); //should print out false
+		 System.out.println(p1.hasRoot(-1)); //should print out true
+		 System.out.println();
+		 
+		 Polynomial m1 = p4.multiply(p5);
+		 Polynomial m2 = p5.multiply(p4);
+		 
+		 for(double x : m1.coefficients) {
+			 System.out.println(x); //should print out 5.0, 4.0, 14.0, 6.0, 2.0
+		 }
+		 System.out.println();
+		 for(int x: m1.degrees) {
+			 System.out.println(x); //should print out 4, 2, 0, 3, 1
+		 }
+		 System.out.println();
+		 for(double x : m2.coefficients) {
+			 System.out.println(x); //should print out 5.0, 4.0, 14.0, 6.0, 2.0
+		 }
+		 System.out.println();
+		 for(int x: m2.degrees) {
+			 System.out.println(x); //should print out 4, 2, 0, 3, 1
+		 }
+		 System.out.println();
+		 File f1 = new File("C:\\Jerry\\f1.txt");
+		 p4.saveToFile("C:\\Jerry\\f1.txt");
+		 Polynomial pf1 = new Polynomial(f1);
+		 for(double x: pf1.coefficients) {
+			 System.out.println(x);
+		 }
+		 System.out.println();
+		 for(int x: pf1.degrees) {
+			 System.out.println(x);
+		 }
+		 
+		 
+	 } 
+}
--- Polynomial.java
+++ Polynomial.java
@@ -1,44 +1,259 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Polynomial {
 	double[] coefficients;
+	int [] degrees;
 	public Polynomial() {
 		coefficients = new double[1];
 		coefficients[0] = 0;
 	}
-	public Polynomial(double[] array) {
+	public Polynomial(double[] array, int[] array2) {
 		coefficients = new double[array.length];
+		degrees = new int[array2.length];
 		for(int i = 0; i < array.length; i++) {
 			coefficients[i] = array[i];
 		}
+		for(int j = 0; j < array2.length; j++) {
+			degrees[j] = array2[j];
+		}
 	}	
-	public Polynomial add(Polynomial poly) {
-		if(poly.coefficients.length < coefficients.length) {
-			Polynomial sum = new Polynomial(coefficients);
-			for(int i = 0; i < poly.coefficients.length; i++) {
-				sum.coefficients[i] = sum.coefficients[i] + poly.coefficients[i];
+	public Polynomial(File file) {
+		try {
+			Scanner scanner = new Scanner(file);
+			String poly = scanner.nextLine();
+			scanner.close();
+			String [] splitplus = poly.split("\\+");
+			String [] splitminus;
+			String [] splitx;
+			int index = 0;
+			boolean first = true;
+			int [] temp_degrees = new int[poly.length()];
+			double [] temp_coefficients = new double[poly.length()];
+				
+			for(String s : splitplus) {
+				if(s.indexOf('-') != -1) {
+					splitminus = s.split("-");
+					for(int i = 0; i < splitminus.length; i++) {
+						if(splitminus[i].equals("")) {
+							i++;
+							first = false;
+						}
+						if(splitminus[i].indexOf('x') == -1) { //degree is 0
+							if(i != 0 || first == false) { //negative
+								temp_degrees[index] = 0;
+								temp_coefficients[index] = Double.parseDouble(splitminus[i]) * -1;
+								index++;
+							}else { //not negative
+								temp_degrees[index] = 0;
+								temp_coefficients[index] = Double.parseDouble(splitminus[i]);
+								index++;
+							}
+						}else { //degree >= 1 
+							if(i != 0 || first == false) { //negative
+								if(splitminus[i].equals("x")) {
+									temp_degrees[index] = 1;
+									temp_coefficients[index] = -1;
+									index++;
+									continue;
+								}
+								splitx = splitminus[i].split("x");
+								if(splitx.length == 1) { //degree of 1
+									temp_degrees[index] = 1;
+								}else {
+									temp_degrees[index] = Integer.parseInt(splitx[1]);
+								}
+								if(splitx[0].equals("")) { //coefficient 1
+									temp_coefficients[index] = -1;
+								}else {
+									temp_coefficients[index] = Double.parseDouble(splitx[0]) * -1 ;
+								}
+								index++;
+							}else { //non negative
+								
+								if(splitminus[i].equals("x")) {
+										temp_degrees[index] = 1;
+										temp_coefficients[index] = 1;
+										index++;
+										continue;
+								}
+								splitx = splitminus[i].split("x");
+								if(splitx.length == 1) { //degree of 1
+									temp_degrees[index] = 1;
+								}else {
+									temp_degrees[index] = Integer.parseInt(splitx[1]);
+								}
+								if(splitx[0].equals("")) { //coefficient 1
+									temp_coefficients[index] = 1;
+								}else {
+									temp_coefficients[index] = Double.parseDouble(splitx[0]);
+								}
+								index++;
+							}
+						}	
+					}
+				}else { //no -'s
+					if(s.indexOf("x") == -1) { //degree is 0
+						temp_degrees[index] = 0;
+						temp_coefficients[index] = Double.parseDouble(s);
+						index++;
+					}else {
+						splitx = s.split("x");
+						if(splitx.length == 1) { //degree of 1
+							temp_degrees[index] = 1;
+						}else {
+							temp_degrees[index] = Integer.parseInt(splitx[1]);
+						}
+						if(splitx[0].equals("")) {
+							temp_coefficients[index] = 1;
+						}else {
+							temp_coefficients[index] = Double.parseDouble(splitx[0]);
+						}
+						index++;
+					}
+				}
+				
 			}
-			return sum;
+			coefficients = new double[index];
+			degrees = new int [index];
+			for(int i = 0; i < index; i++) {
+				coefficients[i] = temp_coefficients[i];
+				degrees[i] = temp_degrees[i];
+			}
+		}catch(FileNotFoundException e) {
+			System.out.println("Error: File Not Found");
+			e.printStackTrace();
 		}
-		for(int i = 0; i < coefficients.length; i++) {
-			poly.coefficients[i] = poly.coefficients[i] + coefficients[i];
-		}
-		return poly;
-	}
-	public double evaluate(double num) {
-		double counter = coefficients[0];
-		if(coefficients.length <= 1) {
-			return coefficients[0];
-		}
-		for(int i = 1; i < coefficients.length; i++) {
-				counter = counter + coefficients[i] * Math.pow(num, i);
-		}
-		return counter;
 		
 	}
+	
+	public Polynomial add(Polynomial poly) {
+		//find length of new arrays
+		int mutual = 0;
+		for(int i = 0; i < degrees.length; i++) {
+			for(int j = 0; j < poly.degrees.length; j++) {
+				if(degrees[i] == poly.degrees[j]) {
+					mutual++;
+					break;
+				}
+			}
+		}
+		
+		int index = 0;
+		boolean track = true;
+		int [] new_degrees = new int[poly.degrees.length + degrees.length - mutual];
+		double [] new_coefficients = new double[poly.coefficients.length + coefficients.length - mutual];
+		
+		for(int i = 0; i < degrees.length; i++) { //insert this into new arrays
+			new_degrees[index] = degrees[i];
+			new_coefficients[index] = coefficients[i];
+			index++;
+		}
+		
+		for(int i = 0; i < poly.degrees.length; i++) { //insert poly.degrees into new_degrees
+			track = true;
+			for(int j = 0; j < degrees.length; j++) { //check if poly.degrees is in new_degrees
+				if(degrees[j] == poly.degrees[i]) { //if it is in both arrays, then update value in new array
+					new_coefficients[j] = new_coefficients[j] + poly.coefficients[i];
+					track = false;
+					break;
+				}
+			}
+			if(track == true) {
+				new_degrees[index] = poly.degrees[i];
+				new_coefficients[index] = poly.coefficients[i];
+				index++;
+			}
+		}
+		Polynomial sum = new Polynomial(new_coefficients, new_degrees);
+		return sum;
+
+	}
+	
+	public double evaluate(double num) {
+		double counter = 0;
+		for(int i = 0; i < degrees.length; i++) {
+			counter = counter + (coefficients[i] * Math.pow(num, degrees[i]));
+		}
+		return counter;
+	}
+	
 	public boolean hasRoot(double num) {
 		if(evaluate(num) == 0) {
 			return true;
 		}
 		return false;
+	}
+	
+	public Polynomial multiply(Polynomial poly) {
+		int index = 0;
+		boolean check = true;
+		int [] new_degrees = new int[poly.degrees.length * degrees.length];
+		double [] new_coefficients = new double[poly.coefficients.length * coefficients.length];
+		
+		for(int i = 0; i < degrees.length; i++) {
+			for(int j = 0; j < poly.degrees.length; j++) {
+				check = true;
+				for(int k = 0; k < index; k++) { //checks duplicates
+					if(degrees[i] + poly.degrees[j] == new_degrees[k]) {
+						new_coefficients[k] = new_coefficients[k] + (coefficients[i] * poly.coefficients[j]);
+						check = false;
+						break;
+					}
+				}
+				if(check == true) { //if no duplicates, add new index to both arrays
+					new_degrees[index] = degrees[i] + poly.degrees[j];
+					new_coefficients[index] = coefficients[i] * poly.coefficients[j];
+					index++;
+				}
+			}
+		}
+		
+		//get rid of trailing zeros
+		int [] final_degrees = new int[index];
+		double [] final_coefficients = new double[index];
+		
+		for(int i = 0; i < index; i++) {
+			final_degrees[i] = new_degrees[i];
+			final_coefficients[i] = new_coefficients[i];
+		}
+		Polynomial product = new Polynomial(final_coefficients, final_degrees);
+		return product;
+		
+	}
+	
+	public void saveToFile(String file_name) {
+		try {
+			File file = new File(file_name);
+			String str = "";
+			for(int i = 0; i < coefficients.length; i++) {
+				if(coefficients[i] < 0) {
+					str = str + "-";
+				}else if (coefficients[i] > 0 && i != 0) {
+					str = str + "+";
+				}
+				str = str + coefficients[i];
+				if(degrees[i] == 0) {
+					continue;
+				}
+				str = str + "x";
+				if(degrees[i] == 1) {
+					continue;
+				}
+				str = str + degrees[i];
+			}
+			FileWriter writer = new FileWriter(file);
+			writer.write(str);
+			writer.close();
+		}catch(IOException e) {
+			System.out.println("Error: IOException");
+			e.printStackTrace();
+		}
+		
+		
 	}
 	
 }
